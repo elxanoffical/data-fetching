e4df324084650239118d5a614dc74d83e39bb7e5
Elxan <elxan.offical.07@gmail.com>
2025-02-15 17:44:22 +0400

    Loading and Error States

---
+++ app/user/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Errorpage({ error }: { error: Error }) {
+  useEffect(() => {
+    console.log(`${error}`)
+  },[error]);
+  return <div className="flex items-center justify-center h-screen">
+    <div className="text-2xl text-red-500">Error fetching users data</div>
+  </div>;
+}
